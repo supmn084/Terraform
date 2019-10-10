8b31afe38dff0bf34aab098af3ac8138ccee38b2
supmn084 <47087790+supmn084@users.noreply.github.com>
2019-10-09 21:06:14 -0400

    adding variables

--- AWS/Example1/main.tf
+++ AWS/Example1/main.tf
@@ -4,22 +4,29 @@ provider "aws" {
 resource "aws_instance" "test1" {
     ami = "ami-0c55b159cbfafe1f0"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
 
 user_data = <<-EOF
         #!/bin/bash
         echo "Hello, World" > index.html
-        nohup busybox httpd -f -p 8080 &
+        nohup busybox httpd -f -p ${var.server_port} &
         EOF
-resource "aws_security_group" "instance"{
-    name = "terraform-example-instance"
-    ingress {
-        from_port = 8080
-        to_port = 8080
-        protocol ="tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-}
+
     tags = {
         Name = "terraform-example"
     }
     }
+resource "aws_security_group" "instance"{
+    name = "terraform-example-instance"
+    ingress {
+        from_port = var.server_port
+        to_port = var.server_port
+        protocol ="tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+variable "server_port" {
+    description = "The port the server will use for in and oubound http requests"
+    type = number
+    default = 8080
+}
