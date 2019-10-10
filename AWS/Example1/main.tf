cdc07eb8477b08638d0768f5f55c38de61449f1d
supmn084 <47087790+supmn084@users.noreply.github.com>
2019-10-09 20:41:25 -0400

    Updated for access rules over 8080

--- AWS/Example1/main.tf
+++ AWS/Example1/main.tf
@@ -10,7 +10,15 @@ user_data = <<-EOF
         echo "Hello, World" > index.html
         nohup busybox httpd -f -p 8080 &
         EOF
-
+resource "aws_security_group" "instance"{
+    name = "terraform-example-instance"
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol ="tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
     tags = {
         Name = "terraform-example"
     }
