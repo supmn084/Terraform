affb2a3e015690ae1b63d4e1b3d806800b172097
supmn084 <47087790+supmn084@users.noreply.github.com>
2019-10-09 20:37:40 -0400

    Updated user data for AWS Web Server

--- AWS/Example1/main.tf
+++ AWS/Example1/main.tf
@@ -5,6 +5,12 @@ resource "aws_instance" "test1" {
     ami = "ami-0c55b159cbfafe1f0"
     instance_type = "t2.micro"
 
+user_data = <<-EOF
+        #!/bin/bash
+        echo "Hello, World" > index.html
+        nohup busybox httpd -f -p 8080 &
+        EOF
+
     tags = {
         Name = "terraform-example"
     }
