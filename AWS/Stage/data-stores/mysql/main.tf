2d8cffc2db8e5649eaf1ed8ed572af0e6aa1c3bc
supmn084 <47087790+supmn084@users.noreply.github.com>
2019-11-12 21:23:01 -0500

    DB creation

---
+++ AWS/Stage/data-stores/mysql/main.tf
@@ -0,0 +1,26 @@
+provider "aws"{
+    region = "us-east-2"
+}
+
+resource "aws_db_instance" "example" {
+    identifier_prefix = "terraform-up-and-running"
+    engine = "mysql"
+    allocated_storage = "10"
+    instance_class = "db.t2.micro"
+    name = "example_database"
+    username = "admin"
+    password = var.db_password
+}
+#the password will need to be entered in manually, can set by declaring it before runnign with
+#export TF_VAR_db_password = "(YOUR_DB_PASSWORD)" then running terraform apply
+terraform{
+backend "s3"{
+        bucket = "chris-up-and-running-terraform-example"
+        key = "stage-loadster/s3/sqldb/terraform.tfstate"
+        region = "us-east-2"
+    #The above stores the state files into the bucket, location etc    
+    #Dynamo DB Table    
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt = true
+    }
+}
---
+++ AWS/Stage/data-stores/mysql/variables.tf
@@ -0,0 +1,5 @@
+variable "db_password"{
+    description = "the password for my example db"
+    type = string
+    #no default, so this is mandantory to set a password, will prompt 
+}
